ac82eb679f278e86d32dd6067d82eb5bba6f05f9
shreysh-mane <shreysh.mane12@gmail.com>
2021-08-21 12:17:50 +0530

    feat-basic working

---
+++ script.js
@@ -0,0 +1,23 @@
+const dateInput = document.querySelector("#date__input");
+const noInput = document.querySelector("#no__input");
+const checkBtn = document.querySelector("#check__btn");
+const checkAns = document.querySelector("#check__ans");
+
+const calLuck = () => {
+  let arr = dateInput.value.replaceAll("-", "");
+  let luckyNo = noInput.value;
+  let sum = 0;
+  let message = "";
+  for (dob of arr) {
+    dob = Number(dob);
+    sum += dob;
+  }
+  if (sum % luckyNo == 0) {
+    message = "Yess";
+  } else {
+    message = "Noo";
+  }
+
+  checkAns.textContent = message;
+};
+checkBtn.addEventListener("click", calLuck);
